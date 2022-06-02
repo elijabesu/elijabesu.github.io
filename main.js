d6d0a6a119bbe848a6763867021e1a584cc90b4d
elijabesu <41390133+elijabesu@users.noreply.github.com>
2022-06-02 12:54:05 +0200

    Mobile menu

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-function toggleMobileMenu(menu) {
-    menu.classList.toggle("open");
+function toggleMobileMenu() {
+    document.getElementsByClassName("hamburger-icon")[0].classList.toggle("open");
+    document.getElementsByClassName("menu")[0].classList.toggle("open");
 }
+
+$(function(){
+    $(".hamburger-icon").on("click", toggleMobileMenu);
+    $(".menu li").on("click", toggleMobileMenu);
+});
