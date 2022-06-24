a61fa70038bf15b453bee20dda6e88b39f21e995
Eliska Faltynkova <eliska.faltynkova@sanezoo.com>
2022-06-24 14:03:11 +0200

    minor changes

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 function toggleMobileMenu() {
-    document.getElementsByClassName("hamburger-icon")[0].classList.toggle("open");
+    document
+        .getElementsByClassName("hamburger-icon")[0]
+        .classList.toggle("open");
     document.getElementsByClassName("menu")[0].classList.toggle("open");
 }
 
-$(function(){
+$(function () {
     $(".hamburger-icon").on("click", toggleMobileMenu);
     $(".menu li").on("click", toggleMobileMenu);
 });
